e38cf5ebcf0df5b8338313e4d2269612d9787b26
nrdygrnma <github@nrdygrnma.tech>
2024-10-12 14:31:03 +0200

    added shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@pinia/nuxt'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@pinia/nuxt",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
   googleFonts: {
     families: {
       Roboto: true,
       Raleway: {
         wght: [100, 300, 400, 700],
-        ital: [100]
-      }
-    }
-  }
-})
+        ital: [100],
+      },
+    },
+  },
+});
